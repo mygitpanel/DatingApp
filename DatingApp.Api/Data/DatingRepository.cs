f7456564555823265d6c547356cb011fb4e8a47a
Sharma <rahul.sharma2@dxc.com>
2020-03-16 08:43:28 +0530

    Dating Repository Interface and implementation done

---
+++ DatingApp.Api/Data/DatingRepository.cs
@@ -0,0 +1,43 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using DatingApp.Api.Controllers.Models;
+using DatingApp.Api.Controllers.Models.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace DatingApp.Api.Data
+{
+    public class DatingRepository : IDatingRepository
+    {
+        private readonly DataContext _context;
+        public DatingRepository(DataContext context)
+        {
+            _context = context;
+        }
+        public void Add<T>(T entity) where T : class
+        {
+            _context.Add(entity);
+        }
+
+        public void Delete<T>(T entity) where T : class
+        {
+            _context.Remove(entity);
+        }
+
+        public async Task<User> GetUser(int id)
+        {
+            var user = await _context.users.Include(p => p.Photo).SingleOrDefaultAsync(u => u.Id == id);
+            return user;
+        }
+
+        public async Task<IEnumerable<User>> GetUsers()
+        {
+            var users = await _context.users.Include(p => p.Photo).ToListAsync();
+            return users;
+        }
+
+        public async Task<bool> SaveAll()
+        {
+            return await _context.SaveChangesAsync() > 0; // return true if savechanges count is greater then 0 else return false;
+        }
+    }
+}
