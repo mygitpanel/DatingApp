048ab79764eda28a08c63777641f1f946aef4525
Sharma <rahul.sharma2@dxc.com>
2020-03-17 22:09:53 +0530

    UserDetailedDTO and UserListDTO Created So that we get information from controller that we want. not all the information we want return.

---
+++ DatingApp.Api/DTOs/UserForDetailedDTO.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections.Generic;
+using DatingApp.Api.Controllers.Models;
+
+namespace DatingApp.Api.DTOs
+{
+    public class UserForDetailedDTO
+    {
+        
+        public int Id { get; set; }
+        public string username{get;set;}
+        public string Gender { get; set; }
+        public int Age { get; set; }
+        public string KnownAs { get; set; }
+        public DateTime Created { get; set; }
+        public DateTime LastActive { get; set; }
+        public string Introduction { get; set; }
+        public string LookingFor { get; set; }
+        public string Interests { get; set; }
+        public string City { get; set; }
+        public string Country { get; set; }
+        public string PhotoUrl { get; set; }
+        public ICollection<Photo> Photo { get; set; }
+    }
+}
